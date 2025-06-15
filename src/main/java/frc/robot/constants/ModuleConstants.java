7f948ee8533293844ae285334dafd8b59f03f217
Aras ARKAN <arasarkan9@gmail.com>
2025-06-15 19:19:47 +0300

    WIP: code for phoenix drive base

--- src/main/java/frc/robot/constants/ModuleConstants.java
+++ src/main/java/frc/robot/constants/ModuleConstants.java
@@ -8,6 +8,7 @@ import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
 
 public class ModuleConstants {
     public static final Current DRIVE_CURRENT_LIMIT = Amps.of(60);
@@ -30,5 +31,10 @@ public class ModuleConstants {
     public static final Constraints ANGLE_CONSTRAINTS = new Constraints(
             RadiansPerSecond.of(1000).in(RadiansPerSecond),
             RadiansPerSecondPerSecond.of(1000).in(RadiansPerSecondPerSecond));
+
+    public static final double DRIVE_GEAR_RATIO = 6.75;
+    public static final double STEER_GEAR_RATIO = 150.0 / 7.0;
+    public static final double WHEEL_RADIUS_METERS = 0.051;
+    public static final Distance WHEEL_RADIUS = null;
 
 }
